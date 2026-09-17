52ea7918e37d8b7ca06f5ac6a8821a8e75c544b9
anadityarout <yourgmail@gmail.com>
2026-09-17 12:56:35 +0530

    wrap main application in React.StrictMode and HelmetProvider for improved performance and SEO

--- frontend/src/Website/src/main.jsx
+++ frontend/src/Website/src/main.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 import Appmain from "./Appmain";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Appmain />
-  </BrowserRouter>
+  <React.StrictMode>
+    <HelmetProvider>
+      <BrowserRouter>
+        <Appmain />
+      </BrowserRouter>
+    </HelmetProvider>
+  </React.StrictMode>
 );
